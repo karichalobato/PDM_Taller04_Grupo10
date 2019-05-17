181ebec844010bee10a2d27d44b118b71d498aff
karichalobato <00002517@uca.edu.sv>
2019-05-17 14:33:34 -0600

    Initial commit

---
+++ app/src/androidTest/java/com/lobato/pdm_taller04_grupo10/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.lobato.pdm_taller04_grupo10
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.lobato.pdm_taller04_grupo10", appContext.packageName)
+    }
+}
