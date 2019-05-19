c031e39c691aa0a87b991cc43a6ac7355ddf1fbe
karichalobato <00002517@uca.edu.sv>
2019-05-19 13:51:04 -0600

    Entidad libro

--- app/src/main/java/com/lobato/pdm_taller04_grupo10/database/entities/Libro.kt
+++ app/src/main/java/com/lobato/pdm_taller04_grupo10/database/entities/Libro.kt
@@ -1,4 +1,23 @@
 package com.lobato.pdm_taller04_grupo10.database.entities
 
-class Libro {
+import android.media.Image
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Libro")
+data class Libro (
+    @ColumnInfo(name = "Portada")
+    val portada: Image,
+    @ColumnInfo(name = "Titulo")
+    val name: String,
+    @ColumnInfo(name = "Autor")
+    val autor: String,
+    @ColumnInfo(name = "Edición")
+    val edicion: String,
+    @ColumnInfo(name = "Descripción")
+    val descripcion: String
+){
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
 }
