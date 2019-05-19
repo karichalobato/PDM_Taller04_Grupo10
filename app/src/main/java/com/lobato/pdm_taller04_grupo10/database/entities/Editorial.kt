a18502feb07b1c7e27a371a6722a80f631d79c1e
karichalobato <00002517@uca.edu.sv>
2019-05-19 14:10:27 -0600

    Entidad editorial

--- app/src/main/java/com/lobato/pdm_taller04_grupo10/database/entities/Editorial.kt
+++ app/src/main/java/com/lobato/pdm_taller04_grupo10/database/entities/Editorial.kt
@@ -1,4 +1,22 @@
 package com.lobato.pdm_taller04_grupo10.database.entities
 
-class Editorial {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Editorial")
+data class Editorial (
+    @ColumnInfo(name = "Nombre")
+    val name: String,
+    @ColumnInfo(name = "Address")
+    val addres: String,
+    @ColumnInfo(name = "PostalCode")
+    val postal_code: Int,
+    @ColumnInfo(name = "Phone")
+    val phone: Number,
+    @ColumnInfo(name = "Email")
+    val email: String
+){
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
 }
--- app/src/main/java/com/lobato/pdm_taller04_grupo10/database/entities/Libro.kt
+++ app/src/main/java/com/lobato/pdm_taller04_grupo10/database/entities/Libro.kt
@@ -5,18 +5,18 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Libro")
+@Entity(tableName = "Book")
 data class Libro (
-    @ColumnInfo(name = "Portada")
-    val portada: Image,
-    @ColumnInfo(name = "Titulo")
-    val name: String,
-    @ColumnInfo(name = "Autor")
-    val autor: String,
-    @ColumnInfo(name = "Edición")
-    val edicion: String,
-    @ColumnInfo(name = "Descripción")
-    val descripcion: String
+    @ColumnInfo(name = "CoverPage")
+    val cover_page: Image,
+    @ColumnInfo(name = "Title")
+    val title: String,
+    @ColumnInfo(name = "Author")
+    val author: String,
+    @ColumnInfo(name = "Edition")
+    val edition: String,
+    @ColumnInfo(name = "Description")
+    val description: String
 ){
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
