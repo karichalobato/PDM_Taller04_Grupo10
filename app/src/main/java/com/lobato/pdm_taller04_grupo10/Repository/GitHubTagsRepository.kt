ee0a62d6dee1f8dbd61fce3c92b50f7277297fa4
fernandom721 <00076617@uca.edu.sv>
2019-05-20 20:52:30 -0600

    Add Autor Repository

---
+++ app/src/main/java/com/lobato/pdm_taller04_grupo10/Repository/GitHubTagsRepository.kt
@@ -0,0 +1,13 @@
+package com.lobato.pdm_taller04_grupo10.Repository
+
+import androidx.annotation.WorkerThread
+import com.lobato.pdm_taller04_grupo10.database.daos.TagsDAO
+import com.lobato.pdm_taller04_grupo10.database.entities.Tags
+
+class GitHubTagsRepository (private val tagsDao: TagsDAO){
+    /*
+    @WorkerThread
+    suspend fun insert(repo: Tags){
+
+    }*/
+}
