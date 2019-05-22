6917669f7f432b2c6a3ac0cdcbcde88174081e2e
Roberto Hernandez <hernandez.roberto014@gmail.com>
2019-05-21 19:47:35 -0600

    Se creo la clase ViewModel. Se comenzara a trabajar!!

---
+++ app/src/main/java/com/lobato/pdm_taller04_grupo10/database/viewmodel/BookViewModel.kt
@@ -0,0 +1,8 @@
+package com.lobato.pdm_taller04_grupo10.database.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class BookViewModel(app:Application):AndroidViewModel(app){
+
+}
