849ecc5c441483dc508d2b01370795ecdc09d782
RodrigoCruz1154 <00115417@uca.edu.sv>
2019-05-25 22:39:32 -0600

    Reestructuraciòn del Adapter, falta implemetar mètodo que junte todos los objetos en uno en el viewmodel

--- app/src/main/java/com/lobato/pdm_taller04_grupo10/activities/MainActivity.kt
+++ app/src/main/java/com/lobato/pdm_taller04_grupo10/activities/MainActivity.kt
@@ -2,13 +2,18 @@ package com.lobato.pdm_taller04_grupo10.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.lobato.pdm_taller04_grupo10.R
 import com.lobato.pdm_taller04_grupo10.adapters.BookAdapter
+import com.lobato.pdm_taller04_grupo10.viewmodel.BookViewModel
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bookViewModel: BookViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +23,17 @@ class MainActivity : AppCompatActivity() {
         val adapter = BookAdapter(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        bookViewModel = ViewModelProviders.of(this).get(BookViewModel::class.java)
+
+        bookViewModel.getAllLibros().observe(this, Observer{ books->
+            books?.let { adapter.setBooks(it) }
+        })
+        bookViewModel.getAllAutor().observe(this, Observer{autor->
+            autor?.let { adapter.setAuthors(it)}
+        })
+        bookViewModel.getAllTags().observe(this,Observer{tags->
+            tags?.let{adapter.setTags(it)}
+        })
     }
 }
--- app/src/main/java/com/lobato/pdm_taller04_grupo10/adapters/BookAdapter.kt
+++ app/src/main/java/com/lobato/pdm_taller04_grupo10/adapters/BookAdapter.kt
@@ -6,10 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.lobato.pdm_taller04_grupo10.R
 import com.lobato.pdm_taller04_grupo10.database.entities.Libro
 import com.lobato.pdm_taller04_grupo10.database.entities.Tags
 import com.lobato.pdm_taller04_grupo10.database.entities.Autor
+import com.lobato.pdm_taller04_grupo10.database.entities.Editorial
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.book_view_land.view.*
 
 class BookAdapter internal constructor(context: Context): RecyclerView.Adapter<BookAdapter.BookViewHolder>(){
 
@@ -17,15 +21,37 @@ class BookAdapter internal constructor(context: Context): RecyclerView.Adapter<B
     private var books = emptyList<Libro>()
     private var authors = emptyList<Autor>()
     private var tags = emptyList<Tags>()
+    private var editorial = emptyList<Editorial>()
+
 
     inner class BookViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val CurrentBookEdition : TextView = itemView.findViewById(R.id.edicion)
+        fun bindBook(book:Libro)=with(itemView){
+            Glide.with(itemView.context)
+                .load(book.cover_page)
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(portada)
+            titulo.text = book.title
+            edicion.text = book.edition.toString()
+            isbn.text = book.isbn
+        }
+        fun bindAutor(book:Autor)=with(itemView){
+            autores.text = book.name
+        }
+        fun bindEditorial(book:Editorial)=with(itemView){
+            imprenta.text = book.name
+
+        }
+        fun bindTags(book:Tags)=with(itemView){
+            tags.text = book.name
+
+        }
+       /* val CurrentBookEdition : TextView = itemView.findViewById(R.id.edicion)
         val CurrentBookTitle : TextView = itemView.findViewById(R.id.titulo)
         val Resume : TextView = itemView.findViewById(R.id.resumen)
         val CurrentEditorial : TextView = itemView.findViewById(R.id.imprenta)
         val ISBN : TextView = itemView.findViewById(R.id.isbn)
         val Authors : TextView = itemView.findViewById(R.id.autores)
-        val Tags : TextView = itemView.findViewById(R.id.tags)
+        val Tags : TextView = itemView.findViewById(R.id.tags)*/
 
     }
 
@@ -35,21 +61,38 @@ class BookAdapter internal constructor(context: Context): RecyclerView.Adapter<B
     }
 
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+        holder.bindBook(books[position])
+        holder.bindAutor(authors[position])
+        holder.bindTags(tags[position])
+        holder.bindEditorial(editorial[position])
+
+       /*
         val current = books[position]
+
         val actual = authors[position]
         val puntero = tags[position]
+        val saltin = editorial[position]
         holder.CurrentBookTitle.text = current.title
-        holder.CurrentBookEdition.text = current.edition.toString()
+        holder.CurrentBookEdition.text = saltin.name
         holder.Resume.text = current.description
         holder.CurrentEditorial.text = current.editorial.toString()
         holder.ISBN.text = current.isbn
         holder.Authors.text = actual.name
-        holder.Tags.text = puntero.name
+        holder.Tags.text = puntero.name*/
+
     }
 
-    internal fun setBooks(books: List<Libro>, authors: List<Autor>, tags:List<Tags>) {
+    internal fun setBooks(books: List<Libro>) {
         this.books = books
+        notifyDataSetChanged()
+    }
+
+    internal fun setAuthors(authors:List<Autor>){
         this.authors = authors
+        notifyDataSetChanged()
+    }
+
+    internal fun setTags(tags:List<Tags>){
         this.tags = tags
         notifyDataSetChanged()
     }
